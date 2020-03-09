bcf1e92722d1718dffa65f90d4c571bc4949abf6
Josh Crites <joshcrites@Joshs-MacBook-Pro-2.local>
2020-03-09 15:24:53 -0400

    handle no .secret file

--- utils/getAccount.js
+++ utils/getAccount.js
@@ -7,21 +7,21 @@ const filePath = path.join(__dirname, '../.secret');
 
 function getAccount() {
     return new Promise(resolve => {
-        fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
-            if(data.length == 0){
-                let randomAccount = web3.eth.accounts.create()
-        
-                fs.writeFile(filePath, randomAccount.privateKey, (err) => {
-                    if(err) {
-                        return console.log(err);
-                    }
-                })
-
-                resolve(randomAccount)
-            } else {
+        if(fs.existsSync(filePath)){
+            fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
                 resolve(web3.eth.accounts.privateKeyToAccount(data))
-            }
-        })
+            })
+        } else {
+            let randomAccount = web3.eth.accounts.create()
+        
+            fs.writeFile(filePath, randomAccount.privateKey, (err) => {
+                if(err) {
+                    return console.log(err);
+                }
+            })
+
+            resolve(randomAccount)
+        }
     })
 }
 
