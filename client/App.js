a10417b6daa3d68a5393355ee3492860a052e390
Josh <critesjosh@gmail.com>
2020-05-19 17:55:20 -0400

    add code comments

--- client/App.js
+++ client/App.js
@@ -9,7 +9,6 @@ import {
   waitForAccountAuth,
   FeeCurrency
 } from '@celo/dappkit'
-import { CeloContract } from '@celo/contractkit'
 import { toTxResult } from "@celo/contractkit/lib/utils/tx-result";
 import { Linking } from 'expo'
 import HelloWorldContract from './contracts/HelloWorld.json'
@@ -19,6 +18,7 @@ YellowBox.ignoreWarnings(['Warning: The provided value \'moz', 'Warning: The pro
 
 export default class App extends React.Component {
 
+  // Set the defaults for the state
   state = {
     address: 'Not logged in',
     phoneNumber: 'Not logged in',
@@ -28,44 +28,71 @@ export default class App extends React.Component {
     textInput: ''
   }
 
+  // This function is called when the page successfully renders
   componentDidMount = async () => {
+    
+    // Check the Celo network ID
     const networkId = await web3.eth.net.getId();
+    
+    // Get the deployed HelloWorld contract info for the appropriate network ID
     const deployedNetwork = HelloWorldContract.networks[networkId];
 
+    // Create a new contract instance with the HelloWorld contract info
     const instance = new web3.eth.Contract(
       HelloWorldContract.abi,
       deployedNetwork && deployedNetwork.address
     );
 
+    // Save the contract instance
     this.setState({ helloWorldContract: instance })
   }
 
   login = async () => {
+    
+    // A string you can pass to DAppKit, that you can use to listen to the response for that request
     const requestId = 'login'
+    
+    // A string that will be displayed to the user, indicating the DApp requesting access/signature
     const dappName = 'Hello Celo'
+    
+    // The deeplink that the Celo Wallet will use to redirect the user back to the DApp with the appropriate payload.
     const callback = Linking.makeUrl('/my/path')
   
+    // Ask the Celo Alfajores Wallet for user info
     requestAccountAddress({
       requestId,
       dappName,
       callback,
     })
   
+    // Wait for the Celo Wallet response
     const dappkitResponse = await waitForAccountAuth(requestId)
 
+    // Set the default account to the account returned from the wallet
     kit.defaultAccount = dappkitResponse.address
 
+    // Get the stabel token contract
     const stableToken = await kit.contracts.getStableToken()
 
-    const [cUSDBalanceBig, cUSDDecimals] = await Promise.all([stableToken.balanceOf(kit.defaultAccount), stableToken.decimals()])
+    // Get the user account balance (cUSD)
+    const cUSDBalanceBig = await stableToken.balanceOf(kit.defaultAccount)
+    
+    // Convert from a big number to a string
     let cUSDBalance = cUSDBalanceBig.toString()
-    this.setState({ cUSDBalance, isLoadingBalance: false })
-
-    this.setState({ address: dappkitResponse.address, phoneNumber: dappkitResponse.phoneNumber })
+    
+    // Update state
+    this.setState({ cUSDBalance, 
+                    isLoadingBalance: false,
+                    address: dappkitResponse.address, 
+                    phoneNumber: dappkitResponse.phoneNumber })
   }
 
   read = async () => {
+    
+    // Read the name stored in the HelloWorld contract
     let name = await this.state.helloWorldContract.methods.getName().call()
+    
+    // Update state
     this.setState({ contractName: name })
   }
 
@@ -74,8 +101,10 @@ export default class App extends React.Component {
     const dappName = 'Hello Celo'
     const callback = Linking.makeUrl('/my/path')
 
+    // Create a transaction object to update the contract with the 'textInput'
     const txObject = await this.state.helloWorldContract.methods.setName(this.state.textInput)
 
+    // Send a request to the Celo wallet to send an update transaction to the HelloWorld contract
     requestTxSig(
       kit,
       [
@@ -89,8 +118,11 @@ export default class App extends React.Component {
       { requestId, dappName, callback }
     )
 
-    const dappkitResponse = await waitForSignedTxs(requestId);
-    const tx = dappkitResponse.rawTxs[0];
+    // Get the response from the Celo wallet
+    const dappkitResponse = await waitForSignedTxs(requestId)
+    const tx = dappkitResponse.rawTxs[0]
+    
+    // Get the transaction result, once it has been included in the Celo blockchain
     let result = await toTxResult(kit.web3.eth.sendSignedTransaction(tx)).waitReceipt()
 
     console.log(`Hello World contract update transcation receipt: `, result)  
