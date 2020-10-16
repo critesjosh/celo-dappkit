084a6e9a4f941e608387da5c10ede88ff5179db3
Josh <critesjosh@gmail.com>
2020-10-16 13:04:24 -0400

    Update metro.config.js

--- client/metro.config.js
+++ client/metro.config.js
@@ -8,7 +8,9 @@ module.exports = {
       fs: require.resolve("expo-file-system"),
       http: require.resolve("stream-http"),
       https: require.resolve("https-browserify"),
+      net: require.resolve("react-native-tcp"),
       os: require.resolve("os-browserify/browser.js"),
+      path: require.resolve("path-browserify"),
       stream: require.resolve("readable-stream"),
       vm: require.resolve("vm-browserify")
     }
